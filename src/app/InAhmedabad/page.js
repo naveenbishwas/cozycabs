13e33cd1aee147ae51d7194bc1e51322232c4724
naveenbishwas <naveenbishwas4@gmail.com>
2025-09-24 10:40:33 +0530

    ahmedabad change

--- src/app/InAhmedabad/page.js
+++ src/app/InAhmedabad/page.js
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Header from "../Components/Header/page";
 import Footer from "../Components/Footer/page";
 
+/// Ahmedabad
 const InAhmedabad = () => {
   const [openIndex, setOpenIndex] = useState(null);
 
