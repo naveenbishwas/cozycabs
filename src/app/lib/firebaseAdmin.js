adfba2091a5fff80587ed6507cc5f22de5be91f8
Shubham Singh <shubhamkumar2452004@gmail.com>
2026-05-04 13:25:50 +0530

    update new email verification

--- src/app/lib/firebaseAdmin.js
+++ src/app/lib/firebaseAdmin.js
@@ -1,9 +1,22 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../../firebase-admin.json" assert { type: "json" };
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
 if (!admin.apps.length) {
+  if (
+    !process.env.FIREBASE_PROJECT_ID ||
+    !process.env.FIREBASE_CLIENT_EMAIL ||
+    !privateKey
+  ) {
+    throw new Error("Firebase ENV missing");
+  }
+
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey,
+    }),
   });
 }
 
